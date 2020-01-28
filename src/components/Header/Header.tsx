db86fbf64ada931ced80a6a72911d8a83db19b2a
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2020-01-28 22:36:23 +0100

    :sparkles: add auth

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,30 @@ import React, { FC } from "react";
 import styles from "./Header.module.css";
 import { Header, HeaderLink } from "hilfling-gui/lib";
 
-const App: FC<{}> = () => {
+interface Props {
+  login: () => void;
+  logout: () => void;
+  authenticated: boolean | null;
+}
+
+const HeaderComponent: FC<Props> = ({
+  authenticated,
+  login,
+  logout
+}: Props) => {
   return (
     <div className={styles.header}>
       <Header>
         <HeaderLink>BILDER</HeaderLink>
         <HeaderLink>OM OSS</HeaderLink>
-        <HeaderLink>LOGG INN</HeaderLink>
+        {authenticated ? (
+          <HeaderLink onClick={logout}>LOGG UT</HeaderLink>
+        ) : (
+          <HeaderLink onClick={login}>LOGG INN</HeaderLink>
+        )}
       </Header>
     </div>
   );
 };
 
-export default App;
+export default HeaderComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store/index";
 import "./index.css";
-
 import Routes from "./Routes";
 
 const store = configureStore();
