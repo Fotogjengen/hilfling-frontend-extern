7083d582fb6df04d6da3d4e8f3c1785a72e925e7
Pernille Klevstuen <peklevstuen@gmail.com>
2020-02-07 19:50:59 +0100

    Added carousel item

--- src/components/Frontpage/Carousel/Carousel.tsx
+++ src/components/Frontpage/Carousel/Carousel.tsx
@@ -1,8 +1,31 @@
-import React, { FC } from "react";
-import styles from "./Carousel.module.css";
+import React, { FC, useEffect, useState } from "react";
+import { Carousel as CarouselComponent, CarouselItems } from "hilfling-gui/lib";
+import { BaseCarouselItem } from "hilfling-gui/lib/types";
 
 const Carousel: FC<{}> = () => {
-  return <div className={styles.test}></div>;
+  const [items, setItems] = useState<BaseCarouselItem[]>([]);
+
+  useEffect(() => {
+    setItems([
+      {
+        title: "hei",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgGfL49kTB0zjHmH-pV8rK8-QWcRawdK7qASmj32j1BnWZl2UO&s"
+      },
+      {
+        title: "hei2",
+        image:
+          "http://images6.fanpop.com/image/photos/39900000/IMG-6250-PNG-kion-39961687-1024-577.png"
+      }
+    ]);
+  }, []);
+  return (
+    <div>
+      <CarouselComponent delay={5000} height={600} width={900}>
+        {CarouselItems({ width: 900, height: 600, items })}
+      </CarouselComponent>
+    </div>
+  );
 };
 
 export default Carousel;
