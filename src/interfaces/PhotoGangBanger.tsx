226a89770590b443cfc27cc2350aba3773f4bbcd
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-02-08 18:41:37 +0100

    :100: interface added: photogangbanger

---
+++ src/interfaces/PhotoGangBanger.tsx
@@ -0,0 +1,11 @@
+import { BaseInterface } from './BaseInterface';
+
+export interface PhotoGangBanger extends BaseInterface {
+  relationshipAdvice: string;
+  semesterStart: string;
+  active: boolean;
+  pang: boolean;
+  address: string;
+  zipCode: number;
+  city: string;
+}
