c9f6e3015e1637c1aeb478c663a5b0b21a1497c4
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-02-09 17:41:49 +0100

    :ok: added photoTagApi and method, getAll()

--- src/utils/api/api.tsx
+++ src/utils/api/api.tsx
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_BASE_URL = process.env.BASE_URL;
 
 const HEADERS = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json'
+  Accept: "application/json",
+  "Content-Type": "application/json"
 };
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: HEADERS
 });
 
 export async function getTest(): Promise<string> {
   return api
-    .get('/')
-    .then(res => res.data['test'])
+    .get("/")
+    .then(res => res.data["test"])
     .catch(e => console.error(e));
 }
--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,24 +1,30 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import guistyles from "hilfling-gui/lib/styles/utilities.module.css";
 import Header from "../../components/Header/Header";
 import Carousel from "../../components/Frontpage/Carousel/Carousel";
 import CardInformationSection from "../../components/Frontpage/CardInformationSection/CardInformationSection";
 import styles from "./App.module.css";
 import { Footer } from "hilfling-gui/lib";
-// import { withAuth } from "@okta/okta-react";
-// import { useAuth } from "../../utils/auth";
+import { withAuth } from "@okta/okta-react";
+import { useAuth } from "../../utils/auth";
+import { PhotoTagApi } from "../../utils/api/PhotoTagApi";
 
-const App: FC<{}> = () => {
-  //const App: FC<{}> = ({ auth }: any) => {
-  //   const { authenticated } = useAuth(auth);
+const App: FC<{}> = ({ auth }: any) => {
+  const { authenticated } = useAuth(auth);
 
-  //   const login = async () => {
-  //     auth.login("/");
-  //   };
+  // Example call to backend
+  // TODO: Remove this
+  PhotoTagApi.getAll()
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
 
-  //   const logout = async () => {
-  //     auth.logout("/");
-  //   };
+  const login = async () => {
+    auth.login("/");
+  };
+
+  const logout = async () => {
+    auth.logout("/");
+  };
 
   return (
     <div className={styles.contentContainer}>
@@ -35,5 +41,4 @@ const App: FC<{}> = () => {
   );
 };
 
-// export default withAuth(App);
-export default App;
+export default withAuth(App);
