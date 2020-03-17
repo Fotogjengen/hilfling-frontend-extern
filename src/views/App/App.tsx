7d701c5f1791f86493aae9ab02e20dd2fbdecc80
oscarsb <oscarsb@stud.ntnu.no>
2020-03-17 19:36:07 +0100

    Fixing tabs

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -5,10 +5,12 @@ import styles from "./App.module.css";
 import { PhotoTagApi } from "../../utils/api/PhotoTagApi";
 import { withAuth } from "@okta/okta-react";
 import { PhotoTag } from "../../interfaces/PhotoTag";
+import ContentTabs from "../../components/ContentTabs";
 
 const App: FC<{}> = ({ auth }: any) => {
   // Example call to backend
   // TODO: Remove this
+  /*
   PhotoTagApi.getAll()
     .then(res => console.log(res))
     .catch(err => console.log(err));
@@ -23,6 +25,7 @@ const App: FC<{}> = ({ auth }: any) => {
   PhotoTagApi.create(phototag)
     .then(res => console.log(res))
     .catch(err => console.log(err.response.data));
+    */
   // -----API Example done-----
 
   const login = async () => {
@@ -38,7 +41,9 @@ const App: FC<{}> = ({ auth }: any) => {
         <div className="slideshow">
           <Carousel></Carousel>
         </div>
-        <div className="arrangements"></div>
+        <div className="arrangements">
+          <ContentTabs activeTab={"samfundet"}></ContentTabs>
+        </div>
       </div>
       <div className={styles.rightSide}>
         <CardInformationSection />
