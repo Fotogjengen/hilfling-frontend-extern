6bbc6f31a3d74c4b18a33ca58babf289c2a3630c
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-03-25 17:50:33 +0100

    :100: Tests with typescript now works

---
+++ src/views/App/App.test.tsx
@@ -0,0 +1,9 @@
+import App from "./App";
+import React from "react";
+import { shallow } from "enzyme";
+describe("App does load", () => {
+  it("Testing if testing is working", () => {
+    const component = shallow(<App></App>);
+    expect(true).toEqual(true);
+  });
+});
