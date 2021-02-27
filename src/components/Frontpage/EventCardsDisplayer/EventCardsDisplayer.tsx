d166ca2625ccc275850e1da7227e362ab7808e56
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-02-27 13:07:51 +0100

    material ui tabs in frontpage

--- src/components/Frontpage/EventCardsDisplayer/EventCardsDisplayer.tsx
+++ src/components/Frontpage/EventCardsDisplayer/EventCardsDisplayer.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from "react";
-import styles from "./EventCardsDisplayer.module.css";
+import React, { ChangeEvent, FC, useState } from "react";
 import {
   GuiCardPreamble,
   GuiCardTitle,
   GuiImageCard,
 } from "../../../gui-components";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+import TabPanel from "../../TabPanel/TabPanel";
 
 interface Props {
   title?: string;
@@ -15,7 +16,9 @@ interface Props {
 }
 
 const EventCardsDisplayer: FC<Props> = () => {
-  const imageCards = [
+  const [value, setValue] = useState<number>(0);
+
+  const imageCardsSamf = [
     "Edgar",
     "Daglighallen",
     "Strossa",
@@ -40,7 +43,83 @@ const EventCardsDisplayer: FC<Props> = () => {
       </GuiImageCard>
     );
   });
-  return <div className={styles.container}>{imageCards}</div>;
+
+  const imageCardsIsfit = ["Et flott sted ISFiT liker"].map(
+    (placeName, index) => {
+      return (
+        <GuiImageCard
+          key={`image-card-${index}`}
+          placement={"left"}
+          type="samfundet"
+          image={"https://www.w3schools.com/css/img_lights.jpg"}
+        >
+          <GuiCardTitle capitalized title={"Temafest: Gjøre verden bedre"} />
+          <GuiCardPreamble
+            color="red"
+            date="12.10.2020"
+            images={123}
+            location={placeName}
+            type={"EventCard"}
+          />
+        </GuiImageCard>
+      );
+    },
+  );
+
+  const imageCardsUka = ["Fæffæs lommebok", "BI"].map((placeName, index) => {
+    return (
+      <GuiImageCard
+        key={`image-card-${index}`}
+        placement={"left"}
+        type="samfundet"
+        image={"https://www.w3schools.com/css/img_lights.jpg"}
+      >
+        <GuiCardTitle capitalized title={"Temafest: Tjene $$$"} />
+        <GuiCardPreamble
+          color="red"
+          date="12.10.2020"
+          images={123}
+          location={placeName}
+          type={"EventCard"}
+        />
+      </GuiImageCard>
+    );
+  });
+
+  const handleChange = (
+    event: ChangeEvent<Record<string, unknown>>,
+    newValue: number,
+  ) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="SAMFUNDET" />
+          <Tab label="ISFIT" />
+          <Tab label="UKA" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        {imageCardsSamf}
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        {imageCardsIsfit}
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        {imageCardsUka}
+      </TabPanel>
+    </>
+  );
 };
 
 export default EventCardsDisplayer;
