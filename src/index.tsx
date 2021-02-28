40e3964bb696c6de38bf852d1afcab6ef41673f9
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-02-28 13:40:12 +0100

    add lint scripts and remove redux (#84)
    * remove redux
    
    * remove redux references
    
    * add lint scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
-import configureStore from "./store/index";
 import "./index.css";
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,22 +8,18 @@ import guistyles from "./styles/utilities.module.css";
 import { GuiFooter } from "./gui-components";
 import HeaderComponent from "./components/Header/Header";
 
-const store = configureStore();
-
 const Root: React.FC = () => (
-  <Provider store={store}>
-    <div>
-      <div className={guistyles.container}>
-        <div className={"container"}>
-          <Router>
-            <HeaderComponent />
-            <Routes />
-          </Router>
-        </div>
+  <>
+    <div className={guistyles.container}>
+      <div className={"container"}>
+        <Router>
+          <HeaderComponent />
+          <Routes />
+        </Router>
       </div>
-      <GuiFooter />
     </div>
-  </Provider>
+    <GuiFooter />
+  </>
 );
 
 render(<Root />, document.getElementById("root"));
