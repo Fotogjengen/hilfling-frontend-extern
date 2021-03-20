53258469294f9229d7ee640c4deb9dda7a756fa9
Oscar <oscarsb1@gmail.com>
2021-03-20 14:16:05 +0100

    Add modal

--- src/components/Temp/MotiveImage.tsx
+++ src/components/Temp/MotiveImage.tsx
@@ -1,15 +1,64 @@
 import React, { FC } from "react";
-import "./style.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
 interface Props {
   id: number;
   image: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const MotiveImage: FC<Props> = ({ image }: Props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="motiveImage">
-      <img src={image} height="200px" width="300px" />
-      <p>Test</p>
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className="motiveImage">
+            <img src={image} height="200px" width="300px" />
+            <p>Test</p>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
 };
