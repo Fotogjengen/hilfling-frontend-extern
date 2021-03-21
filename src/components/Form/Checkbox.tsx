2d25dc102370744826596312797bc5e0b9fd6e96
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-03-21 13:54:55 +0100

    add checkbox form component

---
+++ src/components/Form/Checkbox.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { FieldRenderProps } from "react-final-form";
+import { Checkbox as MuiCheckbox } from "@material-ui/core";
+
+const Checkbox: FC<FieldRenderProps<typeof MuiCheckbox>> = ({
+  input: { checked, name, onChange, restInput },
+  meta,
+  ...rest
+}) => {
+  return (
+    <MuiCheckbox
+      {...rest}
+      name={name}
+      inputProps={restInput}
+      onChange={onChange}
+      checked={checked}
+    />
+  );
+};
+
+export default Checkbox;
