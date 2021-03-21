cd22c38b445b1c50d1e3dbcbe7cec3669d5f4315
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-03-21 13:56:13 +0100

    add photo upload view

--- src/views/Intern/PhotoUpload/PhotoUpload.tsx
+++ src/views/Intern/PhotoUpload/PhotoUpload.tsx
@@ -1,7 +1,86 @@
 import React, { FC } from "react";
+import { ValidationErrors } from "final-form";
+import { Grid } from "@material-ui/core";
+import PhotoUploadForm from "../../../forms/PhotoUploadForm";
+import { useDropzone } from "react-dropzone";
+import cx from "classnames";
+import styles from "./PhotoUpload.module.css";
+import { GuiImageCard } from "../../../gui-components";
+import PhotoUploadPreview from "../../../components/PhotoUploadPreview/PhotoUploadPreview";
+
+interface Values {
+  album: string;
+  date: Date;
+  motive: string;
+  tags: string[];
+  category: string;
+  place: string;
+  securityLevel: string;
+}
+
+interface DragNDropFile extends File {
+  path: string;
+}
+
+const initialValues: Values = {
+  album: "",
+  date: new Date(),
+  motive: "",
+  tags: [],
+  category: "",
+  place: "",
+  securityLevel: "",
+};
 
 const PhotoUpload: FC = () => {
-  return <div>hei</div>;
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    accept: ".jpg,.jpeg,.png",
+  });
+
+  const files = (acceptedFiles as DragNDropFile[]).map(
+    (file: DragNDropFile, index: number) => (
+      <li key={file.path}>
+        <PhotoUploadPreview />
+      </li>
+    ),
+  );
+
+  const onSubmit = (values: Record<string, unknown>) => {
+    console.log("submit");
+    /*console.log(values);*/
+  };
+  const validate = (values: Record<string, unknown>): ValidationErrors => {
+    /*console.log("validate");
+    console.log(values);*/
+    const errors: ValidationErrors = {};
+    return errors;
+  };
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <PhotoUploadForm onSubmit={onSubmit} validate={validate} />
+        </Grid>
+        <Grid item xs={6}>
+          <section>
+            <div
+              {...getRootProps({ className: "dropzone" })}
+              className={cx(styles.dropzone)}
+            >
+              <input {...getInputProps()} />
+              <p>
+                Drag &apos;n&apos; drop some files here, or click to select
+                files
+              </p>
+            </div>
+            <aside>
+              <ul className={styles.noStyleUl}>{files}</ul>
+            </aside>
+          </section>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default PhotoUpload;
