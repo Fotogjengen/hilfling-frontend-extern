727d644234f8579e121abd18de036c92d6747ba6
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-03-21 13:57:04 +0100

    [wip] add preview for photos waiting to be uploaded

---
+++ src/components/PhotoUploadPreview/PhotoUploadPreview.tsx
@@ -0,0 +1,8 @@
+import styles from "./PhotoUploadPreview.module.css";
+import React, { FC } from "react";
+
+const PhotoUploadPreview: FC = () => {
+  return <div>Hei :)</div>;
+};
+
+export default PhotoUploadPreview;
