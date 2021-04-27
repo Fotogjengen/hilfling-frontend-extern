fe3304d9d3b5474defd0ab6db8963691988791ba
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2021-04-27 21:54:21 +0200

    Feature/taheera/arkivsjef page v2 (#125)
    * Laget arkivsjef-page
    
    * Fikset Typography til Material UI
    
    * Fikset Routes til arkivsjef
    
    * Fikset favicon og tittel på nettsiden

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,11 +4,13 @@ import App from "./views/App/App";
 import About from "./views/About/About";
 import MyProfile from "./views/MyProfile/MyProfile";
 import showMotive from "./components/Temp/ShowMotive";
+import Arkivsjef from "./views/Arkivsjef/ArchiveBoss";
 
 const Routes: FC = () => {
   return (
     <>
       <Route exact path="/" component={App} />
+      <Route exact path="/intern/Arkivsjef" component={Arkivsjef} />
       <Route exact path="/motive" component={showMotive} />
       <Route exact path="/about" component={About} />
       <Route exact path="/myprofile" component={MyProfile} />
