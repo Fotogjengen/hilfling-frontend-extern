50f0b583b11417c3b2e2fc1dd4785e6d73f73299
Taheera Ahmed <taheera@hotmail.com>
2021-08-20 15:54:59 +0200

    Fixed 404 page

--- src/components/NotFound/NotFoundIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-
-const NotFoundIcon: FC = () => {
-  return;
-  <></>;
-};
-
-export default NotFoundIcon;
--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from "react";
 import { Typography } from "@material-ui/core";
-import cx from "classnames";
-import guistyles from "../../styles/utilities.module.css";
-import styles from "../MyProfile/MyProfile.module.css";
-
+import styles from "./NotFound.module.css";
+import image from "./404.png";
 const NotFound: FC = () => {
   return (
-    <div className={}>
-      Shoot! Her gikk noe galt... Denne siden ser ikke ut til å eksistere. Hvis
-      det er noe du mener er feil kan du sende en mail til{" "}
+    <div className={styles.container}>
+      <div className={styles.notFound}>
+        <img src={image} />
+        <Typography paragraph={true} variant={"subtitle1"}>
+          Shoot! Her gikk noe galt... Denne siden ser ikke ut til å eksistere.
+          Hvis det er noe du mener er feil kan du sende en mail til
+          <a href={"mailto:fotogjengen@samfundet.no"}>
+            {" "}
+            fotogjengen@samfundet.no
+          </a>
+          . Forsiden finner du <a href={"https://foto.samfundet.no/"}>her</a>.
+        </Typography>
+      </div>
     </div>
   );
 };
