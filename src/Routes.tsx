abc5d672243df761ebfe3c24c3c5f9d5460c4f68
Taheera Ahmed <taheera@hotmail.com>
2021-08-24 18:39:33 +0200

    Added switch components to routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import App from "./views/App/App";
 import About from "./views/About/About";
 import MyProfile from "./views/MyProfile/MyProfile";
@@ -10,12 +10,14 @@ import Arkivsjef from "./views/Arkivsjef/ArchiveBoss";
 const Routes: FC = () => {
   return (
     <>
-      <Route exact path="/" component={App} />
-      <Route exact path="/intern/Arkivsjef" component={Arkivsjef} />
-      <Route exact path="/motive" component={showMotive} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/myprofile" component={MyProfile} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/intern/Arkivsjef" component={Arkivsjef} />
+        <Route exact path="/motive" component={showMotive} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/myprofile" component={MyProfile} />
+        <Route component={NotFound} />
+      </Switch>
     </>
   );
 };
