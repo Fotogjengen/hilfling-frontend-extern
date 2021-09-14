3b1cfe21d9b800f50f137ffd242c4a3c9f244656
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2021-09-14 19:56:24 +0200

    Feature/taheera/new create 404 page (#206)
    * Added 404 image
    
    * Made the 404-page component
    
    * Added Switch component from "react-router-dom"
    
    * Fixed lame linting error in NotFound component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,23 @@
 import React, { FC } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import App from "./views/App/App";
 import About from "./views/About/About";
 import MyProfile from "./views/MyProfile/MyProfile";
 import showMotive from "./components/Temp/ShowMotive";
 import Arkivsjef from "./views/Arkivsjef/ArchiveBoss";
+import NotFound from "./views/NotFound/NotFound";
 
 const Routes: FC = () => {
   return (
     <>
-      <Route exact path="/" component={App} />
-      <Route exact path="/intern/Arkivsjef" component={Arkivsjef} />
-      <Route exact path="/motive" component={showMotive} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/myprofile" component={MyProfile} />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/intern/Arkivsjef" component={Arkivsjef} />
+        <Route exact path="/motive" component={showMotive} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/myprofile" component={MyProfile} />
+        <Route component={NotFound} />
+      </Switch>
     </>
   );
 };
