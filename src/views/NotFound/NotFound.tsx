0547529a2c84eea21c732b99f91d5c85661c9cf6
Taheera Ahmed <taheera@hotmail.com>
2021-09-14 18:36:49 +0200

    Made the 404-page component

---
+++ src/views/NotFound/NotFound.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+import { Typography } from "@material-ui/core";
+import styles from "./NotFound.module.css";
+import image from "./404.png";
+
+const NotFound: FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.notFound}>
+        <img src={image} alt="404" />
+        <Typography paragraph={true} variant={"subtitle1"}>
+          Shoot! Her gikk noe galt... Denne siden ser ikke ut til å eksistere.
+          Hvis det er noe du mener er feil kan du sende en mail til
+          <a href={"mailto:fotogjengen@samfundet.no"}>
+            {" "}
+            fotogjengen@samfundet.no
+          </a>
+          . Forsiden finner du <a href={"https://foto.samfundet.no/"}>her</a>.
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
