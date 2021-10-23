009c428c063ef418383c9af1a3c0c891f20cc30d
Taheera Ahmed <taheera@hotmail.com>
2021-10-23 15:26:14 +0200

    Changed position interface

--- src/interfaces/Position.tsx
+++ src/interfaces/Position.tsx
@@ -1,6 +1,11 @@
 import { BaseModel } from "./BaseModel";
 
 export interface Position extends BaseModel {
+  positionId: {
+    id: string;
+  };
   title: string;
-  email: string;
+  email: {
+    value: string;
+  };
 }
