fa0b5c2c86afd539d4353633ae183ff7901a8900
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-10-24 21:15:41 +0200

    removes unused packages in package.lock json (#248)
    * removes unused packages in package.lock json
    
    * fix checkbox

--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -1,11 +1,3 @@
-import { FieldRenderProps } from "react-final-form";
-import { FormControlProps } from "@material-ui/core";
-
-export interface FormHelperTextWrapperProps<T> extends FieldRenderProps<T> {
-  label: string;
-  formControlProps: FormControlProps;
-}
-
 export type FormFieldProps<T> = Omit<
   T,
   "onChange" | "name" | "error" | "onBlur" | "value"
