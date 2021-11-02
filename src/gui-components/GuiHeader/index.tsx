87f5e40055b1098cf2a272390bee85d46aa499af
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-11-02 18:07:21 +0100

    remove hamburger menu an all references to it (#264)
    * remove hamburger menu an all references to it
    
    * fix jsx

--- src/gui-components/GuiHeader/index.tsx
+++ src/gui-components/GuiHeader/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, ReactNode } from "react";
 import styles from "./GuiHeader.module.css";
 import GuiLogo from "../GuiLogo";
-import GuiHamburger from "../GuiHamburger";
 import { DefaultProps } from "../../types";
 import cx from "classnames";
 import { Link } from "react-router-dom";
@@ -17,14 +16,6 @@ const GuiHeader: FC<Props> = ({ children, className, ...rest }: Props) => (
       <GuiLogo size={60} />
     </Link>
     <div className={cx(styles.childrenContainer, className)} {...rest}>
-      <GuiHamburger
-        menuClicked={() => console.log("click")}
-        color="black"
-        isOpen={false}
-        width={30}
-        height={20}
-        strokeWidth={3}
-      />
       {children}
     </div>
   </div>
