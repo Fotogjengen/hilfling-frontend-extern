584778a3f70823b2b5fb5ea47bd498430cafd659
Taheera Ahmed <taheera@hotmail.com>
2021-11-02 21:05:49 +0100

    ArchiveBossElement: Added return type

--- src/components/Arkivsjef/ArchiveBossElement/ArchiveBossElement.tsx
+++ src/components/Arkivsjef/ArchiveBossElement/ArchiveBossElement.tsx
@@ -1,15 +1,15 @@
 import { Grid } from "@material-ui/core";
-import React from "react";
+import React, { FC } from "react";
 import ArchiveBossOverflow from "../ArchiveBossOverflow/ArchiveBossOverflow";
 
 interface Props {
   /** Index of element when mapped */
   key: number;
-  /** Text to display in the ArchiveBossElem */
+  /** Text to display in the ArchiveBoss */
   text: string | undefined;
 }
 
-const ArchiveBossElement = ({ key, text }: Props) => {
+const ArchiveBossElement: FC<Props> = ({ key, text }: Props) => {
   return (
     <Grid item xs={6} sm={3} key={key}>
       <Grid container direction="row" justify="flex-start" alignItems="center">
