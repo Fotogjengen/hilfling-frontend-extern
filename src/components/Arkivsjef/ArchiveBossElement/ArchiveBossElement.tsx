41d27618ac22a20c279a9455ef483498a1d2ce1f
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2021-11-04 11:17:17 +0100

    Fix/taheera/fetch data from backend to archive boss (#266)
    * Changed the layout of the Accordion a bit
    
    * Moved ArchiveBoss to intern
    
    * Fixed styling
    
    * ArchiveBossElement: Create ArchiveBossElement for the ArchiveBossAccordion
    
    * ArchiveBossOverflow: Removed comment
    
    * About: Removed redundant axiois .get function
    
    * ArchiveBoss: Ting funker
    
    * ArchiveBoss: Fetching categories, places and albums from database
    
    * ArchiveBossElement: Added return type
    
    * tsconfig: Added react-jsx
    
    * package-lock: Changed name of our app
    
    * ArchiveBoss: Fixed code smells
    
    * Creating new package-lock
    
    * Changed name of our app
    
    * Changing the name back bc. rip merge conflict
    
    * fix console errors
    
    Co-authored-by: Caroline Sandsbråten <carolinesandsbraten@gmail.com>

---
+++ src/components/Arkivsjef/ArchiveBossElement/ArchiveBossElement.tsx
@@ -0,0 +1,27 @@
+import { Grid } from "@mui/material";
+import React, { FC } from "react";
+import ArchiveBossOverflow from "../ArchiveBossOverflow/ArchiveBossOverflow";
+
+interface Props {
+  /** Index of element when mapped */
+  key: number | string | null | undefined;
+  /** Text to display in the ArchiveBoss */
+  text: string | undefined;
+}
+
+const ArchiveBossElement: FC<Props> = ({ text }: Props) => {
+  return (
+    <Grid item xs={6} sm={3}>
+      <Grid container direction="row" justifyContent="flex-start" alignItems="center">
+        <Grid item xs={6} sm={4}>
+          <ArchiveBossOverflow />
+        </Grid>
+        <Grid item xs={6} sm={4} alignContent="space-around">
+          {text}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ArchiveBossElement;
---
+++ src/views/Intern/Arkivsjef/ArchiveBoss.tsx
@@ -0,0 +1,88 @@
+import React, { FC, useEffect, useState } from "react";
+import styles from "./Arkivsjef.module.css";
+import ArchiveBossAccordion from "../../../components/Arkivsjef/ArchiveBossAccordion/ArchiveBossAccordion";
+import { Grid, IconButton, Typography } from "@mui/material";
+import { AddCircle } from "@mui/icons-material";
+import { AlbumDto, PlaceDto, CategoryDto } from "../../../../generated";
+import { AlbumApi } from "../../../utils/api/AlbumApi";
+import { PlaceApi } from "../../../utils/api/PlaceApi";
+import { CategoryApi } from "../../../utils/api/CategoryApi";
+import ArchiveBossElement from "../../../components/Arkivsjef/ArchiveBossElement/ArchiveBossElement";
+
+const ArchiveBoss: FC = () => {
+  const [albums, setAlbums] = useState<AlbumDto[]>([]);
+  const [places, setPlaces] = useState<PlaceDto[]>([]);
+  const [categories, setCategories] = useState<CategoryDto[]>([]);
+
+  useEffect(() => {
+    AlbumApi.getAll()
+      .then((res) => setAlbums(res.data.currentList))
+      .catch((e) => console.log(e));
+    PlaceApi.getAll()
+      .then((res) => setPlaces(res.data.currentList))
+      .catch((e) => console.log(e));
+    CategoryApi.getAll()
+      .then((res) => setCategories(res.data.currentList))
+      .catch((e) => console.log(e));
+  }, []);
+
+  const mapAlbums = (albumsCurrentList: AlbumDto[]) => {
+    return albumsCurrentList.map((album: AlbumDto, index: number) => (
+      <ArchiveBossElement text={album.title} key={index} />
+    ));
+  };
+
+  const mapCurrentList = (dtoList: PlaceDto[] | CategoryDto[]) => {
+    return dtoList.map((dto: PlaceDto | CategoryDto, index: number) => (
+      <ArchiveBossElement text={dto.name} key={index} />
+    ));
+  };
+
+  return (
+    <div className={styles.archiveBoss}>
+      <h2> Arkivsjef </h2>
+      <div className={styles.description}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={2} alignContent="center">
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton aria-label="add">
+                <AddCircle className={styles.svgicon} />
+              </IconButton>
+              <Typography>Legg til ny</Typography>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={10}>
+            <Typography>
+              Denne siden er for fotogjengens Arkivsjef. Her kan du legge til,
+              slette, eller endre Album, Kategorier, Steder eller Medium. Vær
+              meget forsiktig med å forandre albumnavn dersom albumet har bilder
+              liggende i seg - det ødelegger mappestrukturen til bildene.
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+      <ArchiveBossAccordion color="#da7777" name="Album">
+        {mapAlbums(albums)}
+      </ArchiveBossAccordion>
+      <ArchiveBossAccordion color="#f3ee78" name="Sted">
+        {mapCurrentList(places)}
+      </ArchiveBossAccordion>
+      <ArchiveBossAccordion color="#9c77da" name="Kategori">
+        {mapCurrentList(categories)}
+      </ArchiveBossAccordion>
+    </div>
+  );
+};
+
+export default ArchiveBoss;
