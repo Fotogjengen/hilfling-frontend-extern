d8d808fa47d66c5e873f3cee9ab7ea4e078fb4f5
Caroline Sandsbråten <carolinesandsbraten@gmail.com>
2021-11-09 22:03:12 +0100

    persistent login

--- src/utils/auth/ProtectedRoute.tsx
+++ src/utils/auth/ProtectedRoute.tsx
@@ -1,14 +1,16 @@
-import React, { ComponentType } from "react";
+import React, { ComponentType, FC } from "react";
 import { Route, RouteProps } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
-
 
 interface Props extends RouteProps {
   component: ComponentType<any>;
 }
 
-const ProtectedRoute = ({ component, ...args }: Props) => (
-  <Route component={withAuthenticationRequired(component)} {...args} />
+const ProtectedRoute: FC<Props> = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component)}
+    {...args}
+  />
 );
 
 export default ProtectedRoute;
