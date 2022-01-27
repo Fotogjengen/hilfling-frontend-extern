e8f9890aabbeab23a9a0eface6591ebd254079f7
Caroline Sandsbråten <carolinesandsbraten@Carolines-MBP.dhcp.wlan.samfundet.no>
2022-01-27 20:57:00 +0100

    Remove commented line of code

--- src/utils/auth/ProtectedRoute.tsx
+++ src/utils/auth/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentType, FC } from "react";
 import { Route, RouteProps } from "react-router-dom";
-//import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 interface Props extends RouteProps {
