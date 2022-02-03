ccf549c415b77fd9ce3bf1ead60959e9981f8206
Oscar <oscarsb1@gmail.com>
2022-02-03 19:46:52 +0100

    Add searchbar

--- src/components/searchPage/Search.tsx
+++ src/components/searchPage/Search.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useState } from "react";
+import React, { FC, useMemo, useState } from "react";
 import styles from "./Search.module.css";
 import { GuiHeader, GuiHeaderLink } from "../../gui-components";
 import { Link } from "react-router-dom";
 import { Grid, Icon, IconButton, Input, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
 import { VisibilityOff } from "@mui/icons-material";
+import SearchIcon from '@mui/icons-material/Search';
 
 // interface Props {
 //   login: () => void;
@@ -19,13 +20,29 @@ const Search = () => {
     setSearch(event.target.value);
   }
 
+  const placeholders = ["Søk etter gårsdagens konsertopplevelse 🤘", 
+   "Finn bilder av deg selv i stigende promille 🍻",
+   "Søk etter gamle minner...",
+   "Søk etter fotogjengens beste bilder 📸",
+   "Finn bilder av crushet ditt 👀"];
+
+  const random = Math.floor(Math.random() * placeholders.length);
+
+  const placeholder = placeholders[random];
+
   return (
     <div className={styles.header}>
       <Grid>
         <div>
-          <TextField fullWidth variant="outlined" onChange={handleChange} >
-            <Icon color="primary">add_circle</Icon>
-          </TextField>
+          <TextField label={placeholder} fullWidth variant="outlined" onChange={handleChange} InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}/>
         </div>
       </Grid>
     </div>
