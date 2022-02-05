49a9ce4915a2687fda03aef55c403688268d3fd3
Taheera Ahmed <taheera@hotmail.com>
2022-02-05 16:16:24 +0100

    MotiveApi: Added getById method

--- src/utils/api/MotiveApi.ts
+++ src/utils/api/MotiveApi.ts
@@ -1,18 +1,23 @@
 import { api } from "./api";
-import {MotiveDto, PhotoGangBangerDto} from "../../../generated";
+import { MotiveDto, PhotoGangBangerDto } from "../../../generated";
 import { PaginatedResult } from "./types";
 
 export const MotiveApi = {
   getAll: async function (): Promise<PaginatedResult<MotiveDto>> {
     return api.get("/motives");
   },
-  getById: async function (id: string): Promise<PaginatedResult<MotiveDto>> {
-    return api.get(`/motives/${id}`);
+  getById: async function (id: string): Promise<MotiveDto> {
+    return api
+      .get(`/motives/${id}`)
+      .then((res) => res.data)
+      .catch((e) => console.log(e));
   },
 };
 
 export const PhotoGangBangerApi = {
-  getAllActive: async function (): Promise<PaginatedResult<PhotoGangBangerDto>> {
+  getAllActive: async function (): Promise<
+    PaginatedResult<PhotoGangBangerDto>
+  > {
     return api.get("/photo_gang_bangers/active");
-  }
-}
+  },
+};
