4edec82f71d2c8b0a516c10418559dbfa5a59ec9
Oscar <oscarsb1@gmail.com>
2022-02-06 15:21:42 +0100

    Change image url for testing

--- src/utils/createImgUrl/createImgUrl.ts
+++ src/utils/createImgUrl/createImgUrl.ts
@@ -2,5 +2,7 @@ import { PhotoDto } from "../../../generated";
 
 /* TODO: Fix this. Idk how */
 export const createImgUrl = (photo: PhotoDto):string => {
-  return `http://localhost:8383/img/FG/${photo.motive?.motiveId.id}/${photo?.largeUrl}`;
+  return `${photo?.largeUrl ? photo.largeUrl : ""}`
+  //Uncomment this when we are starting to use real file storage
+  //return `http://localhost:8383/img/FG/${photo.motive?.motiveId.id}/${photo?.largeUrl}`;
 };
