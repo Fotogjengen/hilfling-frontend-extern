162d647623d172efbceb2bfdbb0f6430802682a4
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2022-03-03 21:12:18 +0100

    PlaceApi, CategoryApi: Added delete by id

--- src/utils/api/CategoryApi.ts
+++ src/utils/api/CategoryApi.ts
@@ -6,4 +6,7 @@ export const CategoryApi = {
   getAll: async function (): Promise<PaginatedResult<CategoryDto>> {
     return api.get("/categories");
   },
+  deleteById: async function (id: string): Promise<number> {
+    return api.delete(`/places/${id}`);
+  },
 };
--- src/utils/api/PlaceApi.ts
+++ src/utils/api/PlaceApi.ts
@@ -3,7 +3,10 @@ import { PlaceDto } from "../../../generated";
 import { PaginatedResult } from "./types";
 
 export const PlaceApi = {
-    getAll: async function (): Promise<PaginatedResult<PlaceDto>> {
-        return api.get("/places");
-    },
+  getAll: async function (): Promise<PaginatedResult<PlaceDto>> {
+    return api.get("/places");
+  },
+  deleteById: async function (id: string): Promise<number> {
+    return api.delete(`/places/${id}`);
+  },
 };
