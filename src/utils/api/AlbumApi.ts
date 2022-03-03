f03c82fae3665accaf849866d29946a3fe6f43af
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2022-03-03 21:09:05 +0100

    AlbumApi: Added delete function

--- src/utils/api/AlbumApi.ts
+++ src/utils/api/AlbumApi.ts
@@ -6,4 +6,7 @@ export const AlbumApi = {
   getAll: async function (): Promise<PaginatedResult<AlbumDto>> {
     return api.get("/albums");
   },
+  deleteById: async function (id: string): Promise<number> {
+    return api.delete(`/albums/${id}`);
+  },
 };
