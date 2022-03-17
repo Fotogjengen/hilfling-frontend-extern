ec106fe76450c09f9ec453602a8fb962cb235888
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2022-03-17 20:21:03 +0100

    Apis: Fixed Api

--- src/utils/api/AlbumApi.ts
+++ src/utils/api/AlbumApi.ts
@@ -2,11 +2,21 @@ import { api } from "./api";
 import { AlbumDto } from "../../../generated";
 import { PaginatedResult } from "./types";
 
+export type DeletedResult = {
+  config: object;
+  headers: object;
+  request: any;
+  status: number;
+  statusText: string;
+  data: number;
+};
+
 export const AlbumApi = {
   getAll: async function (): Promise<PaginatedResult<AlbumDto>> {
     return api.get("/albums");
   },
-  deleteById: async function (id: string): Promise<number> {
-    return api.delete(`/albums/${id}`);
+  deleteById: async function (id: string): Promise<DeletedResult> {
+    return api
+      .delete(`/albums/${id}`)
   },
 };
--- src/utils/api/PlaceApi.ts
+++ src/utils/api/PlaceApi.ts
@@ -1,12 +1,13 @@
 import { api } from "./api";
 import { PlaceDto } from "../../../generated";
 import { PaginatedResult } from "./types";
+import { DeletedResult } from "./AlbumApi";
 
 export const PlaceApi = {
   getAll: async function (): Promise<PaginatedResult<PlaceDto>> {
     return api.get("/places");
   },
-  deleteById: async function (id: string): Promise<number> {
+  deleteById: async function (id: string): Promise<DeletedResult> {
     return api.delete(`/places/${id}`);
   },
 };
