5704fe05d6be7b6bb2e6c656000ad31de2e01715
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2022-09-11 17:56:17 +0200

    Merge with master

--- src/views/Intern/Motives/Motives.tsx
+++ src/views/Intern/Motives/Motives.tsx
@@ -1,10 +1,19 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import Masonry from "@mui/lab/Masonry";
 import React, { useState, useEffect } from "react";
 import { MotiveDto } from "../../../../generated";
+import Search from "../../../components/searchPage/Search";
 import { MotiveApi } from "../../../utils/api/MotiveApi";
-
+import styles from "./Motives.module.css";
+import EditIcon from "@mui/icons-material/Edit";
 /*
-[]
-
+[] Map the res.data.current to cards
 */
 
 const Motives = () => {
@@ -18,9 +27,37 @@ const Motives = () => {
       .catch((e) => console.log(e));
   }, []);
 
+  const mapMotives = (motivesCurrentList: MotiveDto[]) => {
+    return motivesCurrentList.map((motive: MotiveDto, index: number) => (
+      <Card key={motive.motiveId.id}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {motive.categoryDto.name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {motive.title}
+          </Typography>
+          <Typography variant="body2">
+            Eier: {motive.eventOwnerDto.name}
+          </Typography>
+          <Typography variant="body2">Dato: {motive.dateCreated}</Typography>
+        </CardContent>
+        <Button fullWidth size="small" endIcon={<EditIcon />}>
+          Rediger
+        </Button>
+      </Card>
+    ));
+  };
 
-
-  return <div>Motives</div>;
+  return (
+    <div className={styles.motives}>
+      <h2> Motiv </h2>
+      <Search />
+      <Masonry columns={4} spacing={2}>
+        {mapMotives(motives)}
+      </Masonry>
+    </div>
+  );
 };
 
 export default Motives;
