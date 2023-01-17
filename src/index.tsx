7ec1faaccf780eec4af74a084eac709024fd5619
endre2112 <endrerm@samfundet.no>
2023-01-17 21:17:13 +0100

    removed useEffect warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC, useEffect } from "react";
+import React, { useState, FC } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import AppRoutes from "./AppRoutes";
