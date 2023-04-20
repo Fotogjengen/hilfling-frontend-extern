3e6443e9ca8358922df5b03a8b56c7add57e6a09
Marius Bølset Gisleberg <mariubgi@stud.ntnu.no>
2023-04-20 21:45:58 +0200

    begynt på toggle button

--- src/views/Intern/InternSearch/InternSearchView.tsx
+++ src/views/Intern/InternSearch/InternSearchView.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./InternSearch.module.css";
 import InternSearchForm from "./InternSearchForm";
+import { Pagination } from "@mui/material";
+import ToggleComponent from "./ToggleComponent";
+
+const [isGrid, setIsGrid] = useState(true);
+
+const handleChange = () => {
+  setIsGrid(!isGrid);
+};
 
 const InternSearchView = () => {
   return (
     <div className={styles.internSearch}>
+      <ToggleComponent
+        value={isGrid ? "Grid" : "List"}
+        onChange={handleChange}
+      />
+      <Pagination count={10} color="primary" />
       <InternSearchForm />
     </div>
   );
---
+++ src/views/Intern/InternSearch/ToggleComponent.tsx
@@ -0,0 +1,30 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import GridViewIcon from "@mui/icons-material/GridView";
+import ViewListRoundedIcon from "@mui/icons-material/ViewListRounded";
+
+interface ToggleComponentProps {
+  value: string;
+  onChange: (nextView: React.MouseEvent<HTMLElement>) => void;
+}
+
+const ToggleComponent = ({ value, onChange }: ToggleComponentProps) => {
+  return (
+    <div>
+      <ToggleButtonGroup
+        orientation="vertical"
+        value={value}
+        exclusive
+        onChange={onChange}
+      >
+        <ToggleButton value="Grid" aria-label="Grid">
+          <GridViewIcon />
+        </ToggleButton>
+        <ToggleButton value="List" aria-label="List">
+          <ViewListRoundedIcon />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
+};
+
+export default ToggleComponent;
