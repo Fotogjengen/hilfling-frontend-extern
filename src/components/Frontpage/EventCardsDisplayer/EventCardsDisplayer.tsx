9e1d586daec6a2ef5489d6a647964c2501377679
endre2112 <endrerm@samfundet.no>
2023-10-26 17:42:10 +0200

    finished making eventCardDisplay resposnive and cleaned up last code

--- src/components/Frontpage/EventCardsDisplayer/EventCardsDisplayer.tsx
+++ src/components/Frontpage/EventCardsDisplayer/EventCardsDisplayer.tsx
@@ -1,9 +1,5 @@
 import React, { FC, SyntheticEvent, useState } from "react";
-import {
-  GuiCardPreamble,
-  GuiCardTitle,
-  GuiImageCard,
-} from "../../../gui-components";
+
 import { AppBar, Tabs, Tab } from "@mui/material";
 import TabPanel from "../../TabPanel/TabPanel";
 import { useEffect } from "react";
@@ -12,8 +8,7 @@ import { EventOwnerDto, MotiveDto } from "../../../../generated";
 import { MotiveApi } from "../../../utils/api/MotiveApi";
 import { EventOwnerApi } from "../../../utils/api/EventOwnerApi";
 
-
-const EventTypes: string[] = ["Samfundet", "ISFIT", "UKA"];
+import styles from "./EventCardsDisplayer.module.css";
 
 interface Props {
   title?: string;
@@ -37,92 +32,53 @@ const EventCardsDisplayer: FC<Props> = () => {
       .catch((err) => console.error(err));
   }, []);
 
-  const imageCardsSamf = motiveResponse.map((motiveObject, index) => {
-    // TODO: IDK hvordan å løse dette? Muligens heller bruke .filter() jeg vet ikke
-    const samf =
-      motiveObject.eventOwnerDto.name === EventTypes[0] ? true : false;
-    const id = motiveObject.motiveId.id || "default";
-    if (samf) {
-      return (
-        // TODO: Placement, type, location, type and image should be from motiveObject when backend is done
-        <Link key={index} to={`/motive/${id}`}>
-          <GuiImageCard
-            key={`image-card-${index}`}
-            placement="left"
-            type="samfundet"
-            image="https://www.w3schools.com/css/img_lights.jpg"
-          >
-            <GuiCardTitle capitalized title={motiveObject.title} />
-            <GuiCardPreamble
-              color="red"
-              date={motiveObject.dateCreated.toString()}
-              images={69420}
-              location="Blåfjell"
-              type="EventCard"
-            />
-          </GuiImageCard>
-        </Link>
-      );
-    }
-  });
-
-  const imageCardsIsfit = motiveResponse.map((motiveObject, index) => {
-    const samf =
-      motiveObject.eventOwnerDto.name === EventTypes[1] ? true : false;
-    const id = motiveObject.motiveId.id || "default";
-    if (samf) {
-      return (
-        // TODO: Placement, type, location, type and image should be from motiveObject when backend is done
-        <Link key={index} to={`/motive/${id}`}>
-          <GuiImageCard
-            key={`image-card-${index}`}
-            placement="left"
-            type="samfundet"
-            image="https://www.w3schools.com/css/img_lights.jpg"
-          >
-            <GuiCardTitle capitalized title={motiveObject.title} />
-            <GuiCardPreamble
-              color="red"
-              date={motiveObject.dateCreated.toString()}
-              images={69420}
-              location="Blåfjell"
-              type="EventCard"
-            />
-          </GuiImageCard>
-        </Link>
-      );
-    }
-  });
-
-  const imageCardsUka = motiveResponse.map((motiveObject, index) => {
-    const samf =
-      motiveObject.eventOwnerDto.name === EventTypes[2] ? true : false;
-    const id = motiveObject.motiveId.id || "default";
-    if (samf) {
-      return (
-        // TODO: Placement, type, location, type and image should be from motiveObject when backend is done
-        <Link key={index} to={`/motive/${id}`}>
-          <GuiImageCard
-            key={`image-card-${index}`}
-            placement="left"
-            type="samfundet"
-            image="https://www.w3schools.com/css/img_lights.jpg"
-          >
-            <GuiCardTitle capitalized title={motiveObject.title} />
-            <GuiCardPreamble
-              color="red"
-              date={motiveObject.dateCreated.toString()}
-              images={69420}
-              location="Blåfjell"
-              type="EventCard"
-            />
-          </GuiImageCard>
-        </Link>
-      );
-    }
-  });
   const handleChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
+  };
+
+  const Cards = (props: { event: string }) => {
+    const motiveEventResponse = motiveResponse
+      .filter((motiveObject) => {
+        return motiveObject.eventOwnerDto.name === props.event;
+      })
+      .sort((a, b) => {
+        const dateA = new Date(a.dateCreated);
+        const dateB = new Date(b.dateCreated);
+        return dateB.getTime() - dateA.getTime();
+      });
+
+    return (
+      <div className={styles.cardsContainer}>
+        {motiveEventResponse.map((motiveObject, index) => {
+          const id = motiveObject.motiveId.id || "default";
+          return (
+            <Link className={styles.card} key={index} to={`/motive/${id}`}>
+              <img
+                className={styles.cardImg}
+                src="https://www.w3schools.com/css/img_lights.jpg"
+                alt="img"
+              />
+
+              <div className={styles.cardText}>
+                <div className={styles.cardTextTitle}>{motiveObject.title}</div>
+                <div>
+                  <b>Date:</b>
+                  {motiveObject.dateCreated.toString()}
+                </div>
+                <div>
+                  <b>Location:</b>
+                  {"Blåfjell"}
+                </div>
+                <div>
+                  <b>Images:</b>
+                  {"367"}
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
@@ -142,13 +98,13 @@ const EventCardsDisplayer: FC<Props> = () => {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        {imageCardsSamf}
+        <Cards event={"Samfundet"} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        {imageCardsIsfit}
+        <Cards event={"ISFIT"} />
       </TabPanel>
       <TabPanel value={value} index={2}>
-        {imageCardsUka}
+        <Cards event={"UKA"} />
       </TabPanel>
     </>
   );
