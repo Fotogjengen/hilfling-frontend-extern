7ea7a09dfe56fa8dd1a9da304f3c93ae5861e811
PTB03 <ptb_pc@outlook.com>
2024-04-05 17:13:13 +0200

    Tester med å gjøre en liten endring

--- src/components/SearchComponent/SearchField.tsx
+++ src/components/SearchComponent/SearchField.tsx
@@ -11,7 +11,7 @@ const SearchField: FC = () => {
   const [suggestions, setSuggestions] = useState<string[]>([]);
 
   const searchContext = useSearchContext();
-  const setSearchQuery = searchContext ? searchContext.setSearchQuery : () => {console.error("Search context not available"); };
+  const setSearchQuery = searchContext ? searchContext.setSearchQuery : () => {console.error("Search is context not available"); };
   
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
