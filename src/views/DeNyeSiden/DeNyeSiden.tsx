caf65220871b98710add1515beb9cf550f86b7cc
Mathilde1440 <mathilde.skaset.haarr@gmail.com>
2024-11-14 21:56:33 +0100

    Added txt input field

--- src/views/DeNyeSiden/DeNyeSiden.tsx
+++ src/views/DeNyeSiden/DeNyeSiden.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Input, TextField, Box } from '@mui/material';
 
 const DeNyeSiden = () => {
   return (
-    <div>DeNyeSiden</div>
+    <div>
+        <h1>DeNyeSiden</h1>
+              <Box className = "text_input" >
+                <TextField 
+                    // error = {validCountryError}
+                    id="standard-basic" 
+                    label="Brukernavn"
+                    variant="standard" 
+                    helperText = "Skriv inn brukernavnet på alle vennene dine for å spille:)"
+                    // value = {inputValue} 
+                    // onChange = {handleInputChange} 
+                    />
+        
+
+        {/* <button className = "submitButton"> Submit country </button> */}
+        {/* {onClick = {}} */}
+        </Box>
+    </div>
+    
   )
 }
 
